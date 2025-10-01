855180f1aadaa054d332a58badad2ff434d0274c
Jan-Kalla <jk300656@student.polsl.pl>
2025-10-01 14:39:03 +0200

    Initial wedding site setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <h1 className="text-5xl font-bold text-pink-600">Magda & Jan</h1>
+      <p className="mt-4 text-xl">Zapraszamy na nasz ślub! 💍</p>
+    </main>
+  );
+}
