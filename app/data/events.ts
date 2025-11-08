0f69217dc87e677235a104b74082ddaf9c468dad
Jan-Kalla <jk300656@student.polsl.pl>
2025-11-08 15:20:30 +0100

    wydarzenia do timera

---
+++ app/data/events.ts
@@ -0,0 +1,31 @@
+export const events = [
+
+
+{ durationDays: 179, name: "Konklawe 1740 (Najdłuższe konklawe czasów nowożytnych, odbyło się na nim aż 175 głosowań)" },
+{ durationDays: 160, name: "Podróż św. Pawła z Cezarei do Rzymu (Opisana w Dziejach Apostolskich)" },
+
+{ durationDays: 120, name: "Ciąża dzika europejskiego (dzik jest dziki, dzik jest zły!)" },
+{ durationDays: 112, name: "Ciąża wielkiej białej polskiej (Rasa polskiej świni domowej o jednym z najszybszych temp wzrostu)" },
+{ durationDays: 102, name: "Okres od powrotu Napoleona z Elby do klęski pod Waterloo" },
+{ durationDays: 80, name: "Podróż Phileasa Fogga" },
+{ durationDays: 65, name: "Okres inkubacji jaj pingwina cesarskiego" },
+{ durationDays: 52, name: "Czas dojrzewania półtwardego sera gouda (Jong belegen)" },
+{ durationDays: 40, name: "Czas, jaki Jezus spędził na pustyni, kuszony przez diabła" },
+{ durationDays: 33, name: "Ciąża Kangura" },
+{ durationDays: 30, name: "UEFA Euro (Mistrzostwa Europy w piłce nożnej)" },
+{ durationDays: 26, name: "Obrót słońca wkół własnej osi (Bo słońce też się obraca wokół własnej osi, no w sumie logiczne, ale czy ktoś z was się kiedyś zastanawiał nad tym?)" },
+{ durationDays: 19, name: "Pierwsza edycja Tour de France (1903)" },
+{ durationDays: 17, name: "Strajk Generalny Solidarności w 1980" },
+{ durationDays: 15, name: "Letnie Igrzyska Olimpijskie" },
+{ durationDays: 12, name: "Czas kwitnięcia sakury (wiśni japońskiej)" },
+
+{ durationDays: 8, name: "Czas trwania misji Apollo 11" },
+{ durationDays: 6, name: "Światowe Dni Młodzieży w Lizbonie w 2023" },
+{ durationDays: 5, name: "Czas kwitnięcia lotosu" },
+{ durationDays: 4, name: "Rejs Titanica" },
+{ durationDays: 3, name: "Bitwa pod Termopilami" },
+{ durationDays: 2, name: "Erupcja Wezuwiusza w 79 n.e" },
+{ durationDays: 0.8, name: "Lądowanie w Normandii (D-Day)" },
+{ durationDays: 0.6, name: "Pierwszy lot samolotem przez Atlantyk" },
+{ durationDays: 0.1, name: "Koncert Live Aid (2 godziny)" },
+];
